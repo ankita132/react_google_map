a275f6b902e1ee5200c027c450e2cc6833a9ada5
ankita132 <2ankitasahoo13@gmail.com>
2017-06-20 04:42:33 +0530

    markers and pop-up added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import AccessingArgumentsExample from './components/SimpleMapExample';
+
+class App extends Component {
+
+  render() {
+    return (
+    <AccessingArgumentsExample />
+  );
+}
+}
+
+export default App;
---
+++ src/components/SimpleMapExample.js
@@ -0,0 +1,103 @@
+import React,{ Component } from 'react';
+import { withGoogleMap,InfoWindow, GoogleMap, Marker, } from 'react-google-maps';
+
+const google = window.google;
+
+const AccessingArgumentsExampleGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={4}
+    defaultCenter={props.center}
+    onClick={props.onMapClick}
+  >
+    {props.markers.map((marker, index) => (
+            <Marker
+      position={marker.position}
+      key={index}
+      onClick={() => props.onMarkerClick(marker)}
+      >
+      {marker.showInfo && (
+        <InfoWindow onCloseClick={() => props.onMarkerClose(marker)}>
+        <div><h1>cool</h1></div>
+        </InfoWindow>
+      )}
+      </Marker>
+    ))}
+  </GoogleMap>
+));
+
+class AccessingArgumentsExample extends Component{
+  state = {
+   markers: [
+     {
+       position: new google.maps.LatLng(-23.363882, 129.044922),
+       showInfo: false,
+       infoContent: (
+         <h1>cool</h1>
+       ),
+     },
+   ],
+   center: new google.maps.LatLng(-25.363882, 131.044922),
+ };
+
+ handleMapClick = this.handleMapClick.bind(this);
+ handleMarkerClick = this.handleMarkerClick.bind(this);
+ handleMarkerClose = this.handleMarkerClose.bind(this);
+
+ handleMapClick(event) {
+   this.setState({
+     center: event.latLng,
+     markers: [
+       ...this.state.markers,
+       { position: event.latLng },
+     ],
+   });
+ }
+
+ handleMarkerClick(targetMarker) {
+    this.setState({
+      markers: this.state.markers.map(marker => {
+        if (marker === targetMarker) {
+          return {
+            ...marker,
+            showInfo: true,
+          };
+        }
+        return marker;
+      }),
+    });
+  }
+
+  handleMarkerClose(targetMarker) {
+    this.setState({
+      markers: this.state.markers.map(marker => {
+        if (marker === targetMarker) {
+          return {
+            ...marker,
+            showInfo: false,
+          };
+        }
+        return marker;
+      }),
+    });
+  }
+
+ render() {
+   return (
+     <AccessingArgumentsExampleGoogleMap
+       containerElement={
+         <div style={{ height: `600px` }} />
+       }
+       mapElement={
+         <div style={{ height: `600px` }} />
+       }
+       onMapClick={this.handleMapClick}
+       center={this.state.center}
+       markers={this.state.markers}
+       onMarkerClick={this.handleMarkerClick}
+       onMarkerClose={this.handleMarkerClose}
+     />
+   );
+ }
+}
+
+export default AccessingArgumentsExample;
